0cbfe59d3723c97434a35a8431a280bc6dcec4e9
keyboardsage <keyboardsage@users.noreply.github.com>
2024-03-20 07:53:25 -0400

    Now shows the messages as I like

--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,19 @@
+// popup.js
+function formatNumberWithLeadingSpaces(number, targetLength) {
+    let numberString = String(number);
+    let paddingSize = targetLength - numberString.length;
+    let padding = new Array(paddingSize + 1).join('&nbsp;');
+    return padding + numberString;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     chrome.runtime.sendMessage({action: "getTabCount"}, function(response) {
-        document.getElementById('tabCount').textContent = response.number;
+        let formattedNumber = formatNumberWithLeadingSpaces(response.number, 6); // Assuming you won't go over 9999
+        document.getElementById('tabCount').innerHTML = formattedNumber;
     });
 
     chrome.runtime.sendMessage({action: "getMaxTabs"}, function(response) {
-        document.getElementById('maxTabs').textContent = response.number;
+        let formattedNumber = formatNumberWithLeadingSpaces(response.number, 6); // Adjust the 4 based on your needs
+        document.getElementById('maxTabs').innerHTML = formattedNumber;
     });
 });
