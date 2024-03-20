589f4f38d49150198d7abf0dea6798e27f74910f
keyboardsage <keyboardsage@users.noreply.github.com>
2024-03-20 06:50:40 -0400

    Plays the sound using the offscreen doc functionality

--- src/background.js
+++ src/background.js
@@ -1,7 +1,43 @@
+tabCount = 0;
+maxTabs = 2159;
+
+/**
+ * Plays audio files from extension service workers
+ * @param {string} source - path of the audio file
+ * @param {number} volume - volume of the playback
+ */
+async function playSound(source = 'default.wav', volume = 1) {
+    await createOffscreen();
+    await chrome.runtime.sendMessage({ play: { source, volume } });
+}
+
+// create the offscreen document if it doesn't already exist
+async function createOffscreen() {
+    if (await chrome.offscreen.hasDocument()) return;
+    await chrome.offscreen.createDocument({
+        url: 'offscreen.html',
+        reasons: ['AUDIO_PLAYBACK'],
+        justification: 'Used to play the tab preventing sound'
+    });
+}
+
 chrome.tabs.onCreated.addListener((tab) => {
     chrome.tabs.query({}, (tabs) => {
-        if (tabs.length > 2159) {
+        // prevent creating a new tab when maximum is exceeded
+        if (tabs.length > maxTabs) {
             chrome.tabs.remove(tab.id);
+
+            playSound('tab_prevent.wav');
         }
+
+        tabCount = tabs.length;
     });
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.action == "getTabCount") {
+      sendResponse({number: tabCount});
+    }
+  }
+);
---
+++ src/offscreen.js
@@ -0,0 +1,11 @@
+// listen for messages from the extension
+chrome.runtime.onMessage.addListener(msg => {
+    if ('play' in msg) playAudio(msg.play);
+});
+
+// play sound with access to DOM APIs
+function playAudio({ source, volume }) {
+    const audio = new Audio(source);
+    audio.volume = volume;
+    audio.play();
+}
